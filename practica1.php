f71ff8eb134c189d9cf4de8e361e7e70de4b19b7
jesusssGM <124410589+jesusssGM@users.noreply.github.com>
2023-05-22 17:58:13 -0700

    Add files via upload

---
+++ practica1.php
@@ -0,0 +1,27 @@
+<?php 
+
+echo "<h1>hola mundo</h1>";
+echo "<hr>";
+
+$nombre="chuy";
+$edad= 16;
+
+if($edad > 18){
+
+echo $nombre . " es mayor de edad";
+}
+else{
+
+echo $nombre . " es menor de edad";
+
+}
+
+echo "<hr>";
+
+for($i=0;   $i<100; $i++){
+
+
+    echo $i . "<br>";
+}
+
+?>
